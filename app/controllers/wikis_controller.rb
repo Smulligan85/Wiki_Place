9267622d5aaba2cde7e3195546f353db1c3409c5
Sean Mulligan <sean.mulligan85@gmail.com>
2015-04-19 22:41:01 -0700

    Adding Wiki model and controller actions

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,45 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+      if @wiki.save
+        flash[:notice] = "Post was saved."
+        redirect_to @wiki
+      else
+        flash[:error] = "There was an error saving the post. Please try again."
+        render :new
+      end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "Wiki could not be updated. Please try again."
+      render :edit
+    end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body)
+  end
+end
