e4feddbb9e5bc1c350b097bef1dce1e94e980ace
Sean Mulligan <sean.mulligan85@gmail.com>
2015-04-30 19:10:23 -0700

    Getting delete colabs to update

--- app/services/manage_collaborations.rb
+++ app/services/manage_collaborations.rb
@@ -1,7 +1,7 @@
 class ManageCollaborations
 
   def self.call(wiki, user_ids)
-    user_ids.delete('') if user_ids.length > 1
+    user_ids.delete('') if user_ids.length >= 1
     wiki.collaborations.where.not(user_id: user_ids).destroy_all
     user_ids.each { |user_id| Collaboration.create(user_id: user_id, wiki_id: wiki.id) }
   end
