b53e0d046212ec18992a3897395b49b483d8ae83
Sean Mulligan <sean.mulligan85@gmail.com>
2015-10-25 13:50:41 -0700

    Update wiki controller

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -16,7 +16,7 @@ class WikisController < ApplicationController
   end
 
   def create
-    @wiki = Wiki.new(wiki_params)
+    @wiki = current_user.wikis.build(wiki_params)
     authorize @wiki
       if @wiki.save
         flash[:notice] = "Wiki was saved. Huzzah!"
