7cd8aef69ef6121ad17840ffff375ca7e23c18f3
Sean Mulligan <sean.mulligan85@gmail.com>
2015-11-18 19:22:55 -0800

    Update WikisController#index

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,6 +1,6 @@
 class WikisController < ApplicationController
   def index
-    @wikis = Wiki.visible_to(current_user)
+    @wikis = Wiki.all
   end
 
   def show
@@ -16,7 +16,8 @@ class WikisController < ApplicationController
   end
 
   def create
-    @wiki = current_user.wikis.create(wiki_params)
+    @wiki = Wiki.new(wiki_params)
+    @wiki.user = current_user
     authorize @wiki
       if @wiki.save
         flash[:notice] = "Wiki was saved. Huzzah!"
